5a7de28dd50f9c7edffcb693acf0fba0671b12f5
herobrauni <braunnicolaj@gmail.com>
2025-03-21 15:47:07 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,10 +5,11 @@ set -ouex pipefail
 dnf5 -y copr enable yalter/niri-git
 dnf5 -y copr enable ulysg/xwayland-satellite
 dnf5 -y copr enable ryanabx/cosmic-epoch
+dnf5 -y copr enable che/nerd-fonts
 
 dnf5 -y install cosmic-desktop
 
-dnf5 -y install tailscale
+dnf5 -y install tailscale nerd-fonts
 
 dnf5 -y install niri mako waybar swaybg swayidle
 

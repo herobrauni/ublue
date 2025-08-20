ccd0a9c44251288558827c84ebfbb7888034d6c5
herobrauni <braunnicolaj@gmail.com>
2025-08-20 08:30:15 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ dnf5 -y install tailscale nerd-fonts
 
 dnf5 -y install niri mako waybar swaybg swayidle
 
-dnf5 -y install @xfce-desktop-environment
+# dnf5 -y install @xfce-desktop-environment
 
 dnf5 -y install xdg-desktop-portal-gtk xdg-desktop-portal-gnome gnome-keyring polkit-kde xwayland-satellite xwaylandvideobridge
 

d3b9121880a29f2bba13adccdef5f3da6a43b842
herobrauni <braunnicolaj@gmail.com>
2025-10-07 14:38:41 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -45,7 +45,27 @@ dnf5 -y group install development-tools
 
 dnf5 -y remove fish
 
+### COSMIC EXTERNAL NIRI
 curl -L https://raw.githubusercontent.com/Drakulix/cosmic-ext-extra-sessions/refs/heads/main/niri/cosmic-ext-niri.desktop -o /usr/share/wayland-sessions/cosmic-ext-niri.desktop
 mkdir -p /var/usrlocal/bin
 curl -L https://raw.githubusercontent.com/Drakulix/cosmic-ext-extra-sessions/refs/heads/main/niri/start-cosmic-ext-niri -o /var/usrlocal/bin/start-cosmic-ext-niri
 chmod +x /var/usrlocal/bin/start-cosmic-ext-niri
+
+### DMS FOR NIRI
+dnf5 copr enable errornointernet/quickshell
+dnf5 install quickshell-git
+curl -L "https://github.com/google/material-design-icons/raw/master/variablefont/MaterialSymbolsRounded%5BFILL%2CGRAD%2Copsz%2Cwght%5D.ttf" -o /usr/share/fonts/MaterialSymbolsRounded.ttf
+curl -L "https://github.com/rsms/inter/raw/refs/tags/v4.1/docs/font-files/InterVariable.ttf" -o /usr/share/fonts/InterVariable.ttf
+curl -L "https://github.com/tonsky/FiraCode/releases/latest/download/FiraCode-Regular.ttf" -o /usr/share/fonts/FiraCode-Regular.ttf
+
+mkdir /etc/skel/.config/quickshell && git clone https://github.com/AvengeMedia/DankMaterialShell.git /etc/skel/.config/quickshell/dms
+sh -c "curl -L https://github.com/AvengeMedia/danklinux/releases/latest/download/dms-$(uname -m | sed 's/x86_64/amd64/;s/aarch64/arm64/').gz | gunzip | tee /usr/local/bin/dms > /dev/null && chmod +x /usr/local/bin/dms"
+
+dnf5 install cava wl-clipboard brightnessctl
+dnf5 copr enable wef/cliphist && sudo dnf install cliphist
+dnf5 copr enable heus-sueh/packages && sudo dnf install matugen
+
+sh -c "curl -L https://github.com/AvengeMedia/dgop/releases/latest/download/dgop-linux-$(uname -m | sed 's/x86_64/amd64/;s/aarch64/arm64/').gz | gunzip | tee /usr/local/bin/dgop > /dev/null && chmod +x /usr/local/bin/dgop"
+
+
+
